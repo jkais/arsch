4ba6aaab35889a0a3cbd9184c7b7413077cd6b29
Jan Kaiser <kaiser@hey.com>
2025-07-05 15:19:45 +0200

    fixed package

--- arch/install/3-basics.sh
+++ arch/install/3-basics.sh
@@ -3,7 +3,7 @@ yay -S --noconfirm --needed \
   fastfetch btop htop cowsay \
   wget curl unzip inetutils openssh \
   nautilus the_silver_searcher \
-  sddm qt5-graphicaleffects qt5-quickcontrols2 sddm-sugar-dark-git \
+  sddm qt5-graphicaleffects qt5-quickcontrols2 sddm-sugar-dark \
   swayidle \
   vim tig \
   spotify teams-for-linux \
