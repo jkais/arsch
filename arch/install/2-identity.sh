0ab9d151bb03206f15511db77731bfa65f0552b9
Jan Kaiser <kaiser@hey.com>
2025-07-05 20:50:05 +0200

    a little bit cleanup

--- arch/install/2-identity.sh
+++ arch/install/2-identity.sh
@@ -2,6 +2,6 @@
 yay -S --noconfirm --needed gum
 
 # Configure identification
-echo -e "\nEnter identification for git and autocomplete..."
+echo -e "\nEnter identification for Git:"
 export USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
 export USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
--- arch/install/3-basics.sh
+++ arch/install/3-basics.sh
@@ -1,12 +1,12 @@
 yay -S --noconfirm --needed \
   man less whois \
-  fastfetch btop htop cowsay \
-  wget curl unzip inetutils openssh \
+  curl unzip inetutils openssh \
   nautilus the_silver_searcher \
   sddm qt5-graphicaleffects qt5-quickcontrols2 sddm-sugar-dark \
   swayidle \
   vim tig code \
   postman-bin teams-for-linux slack-desktop bruno-bin \
+  fastfetch btop htop cowsay \
   spotify 
 
-  sudo systemctl enable sddm
+sudo systemctl enable sddm
