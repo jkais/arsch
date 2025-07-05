bf96413107865001c24f1792f19dffa5676dabaa
Jan Kaiser <kaiser@hey.com>
2025-07-05 03:34:34 +0200

    first: yay!

--- arch/install.sh
+++ arch/install.sh
@@ -1,1 +1,9 @@
-echo "WURSTI"
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Give people a chance to retry running the installation
+trap 'echo "Installation failed! You can retry by running: source ~/.local/share/arsch/arch/install.sh"' ERR
+
+# Install all scripts
+#
+for f in ~/.local/share/arsch/arch/install/*.sh; do source "$f"; done
