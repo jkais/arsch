ce8d0538a4e12866dd777844b5b9ae2c2cf02b2f
Jan Kaiser <kaiser@hey.com>
2025-07-05 13:27:24 +0200

    added cowsay

--- arch/install/3-basics.sh
+++ arch/install/3-basics.sh
@@ -1,6 +1,7 @@
 yay -S --noconfirm --needed \
+  man less whois \
+  fastfetch btop htop cowsay \
   wget curl unzip inetutils openssh \
   nautilus \
   vim tig \
-  man less whois fastfetch btop \
   spotify teams-for-linux \
