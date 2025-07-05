db986668fa83d0a885f68185e9bd1f1e0e34d358
Jan Kaiser <kaiser@hey.com>
2025-07-05 05:49:02 +0200

    even more hyprland stuff

--- arch/install/hyprland.sh
+++ arch/install/hyprland.sh
@@ -1,6 +1,7 @@
 yay -S --noconfirm --needed \
   ttf-dejavu ttf-liberation noto-fonts \
   fontconfig hyprland \
-  waybar wayland-protocols wl-clipboard
+  waybar wayland-protocols wl-clipboard \
+  wlroots wofi xdg-desktop-portal-hyprland
 
 fc-cache -fv
