617ca6a5c55a5d6f5bac69870848c234c7dbe1c4
Jan Kaiser <kaiser@hey.com>
2025-07-05 15:14:22 +0200

    fiddling with file copying...

--- arch/install/4-config.sh
+++ arch/install/4-config.sh
@@ -11,7 +11,7 @@ SOURCE_DIR="$HOME/.local/share/arsch/config"
 CONFIG_DIR="$HOME/.config"
 cp -r "$SOURCE_DIR"/* "$CONFIG_DIR"
 
-# Copy all share files to /usr/share
-SOURCE_DIR="$HOME/.local/share/arsch/share"
-CONFIG_DIR="/usr/share"
+# Copy all root files to /
+SOURCE_DIR="$HOME/.local/share/arsch/root"
+CONFIG_DIR="/"
 sudo cp -r "$SOURCE_DIR"/* "$CONFIG_DIR"
