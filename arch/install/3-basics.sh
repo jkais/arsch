6b36d6e7275f1bf1e0ce3a1b00d1b6f7a40cfd3c
Jan Kaiser <kaiser@hey.com>
2025-07-05 14:50:26 +0200

    cleanup hyperlock, install swayidle

--- arch/install/3-basics.sh
+++ arch/install/3-basics.sh
@@ -3,5 +3,6 @@ yay -S --noconfirm --needed \
   fastfetch btop htop cowsay \
   wget curl unzip inetutils openssh \
   nautilus the_silver_searcher \
+  swayidle \
   vim tig \
   spotify teams-for-linux \
