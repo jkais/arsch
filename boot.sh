3af5d9f8603f5a5f265b871950b95d428df11823
Jan Kaiser <kaiser@hey.com>
2025-07-05 05:16:17 +0200

    pacman install git before cloning

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
-echo "HERE WE GO!"
+echo "Installing Arsch"
 
-# pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
+pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
 
 rm -rf ~/.local/share/arsch/
 git clone https://github.com/jkais/arsch.git ~/.local/share/arsch >/dev/null
