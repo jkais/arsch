2a538cc7c20f4908a31f4f82de4e372d15e0ae51
Jan Kaiser <kaiser@hey.com>
2025-07-06 17:42:38 +0200

    fixed supression of git checkout

--- boot.sh
+++ boot.sh
@@ -3,6 +3,6 @@ echo "Installing Arsch"
 pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
 
 rm -rf ~/.local/share/arsch/
-git clone https://github.com/jkais/arsch.git ~/.local/share/arsch >/dev/null
+git clone https://github.com/jkais/arsch.git ~/.local/share/arsch > /dev/null 2>&1
 
 source ~/.local/share/arsch/installation/install.sh
