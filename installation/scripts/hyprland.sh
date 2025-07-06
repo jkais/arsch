76ec99348ca787bdcaa896d96119981caeb0d76e
Jan Kaiser <kaiser@hey.com>
2025-07-06 14:18:17 +0200

    install mako

--- installation/scripts/hyprland.sh
+++ installation/scripts/hyprland.sh
@@ -2,8 +2,8 @@ yay -S --noconfirm --needed \
   ttf-dejavu ttf-liberation noto-fonts nerd-fonts-complete noto-fonts-emoji \
   noto-fonts-cjk adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts \
   ttf-font-awesome ttf-material-design-icons \
-  ttf-source-han-sans-jp ttf-source-han-serif-jp \
-  fontconfig hyprland hyprlock \
+  ttf-source-han-sans-jp ttf-source-han-serif-jp fontconfig \
+  hyprland hyprlock mako \
   waybar wayland-protocols wl-clipboard \
   wlroots wofi xdg-desktop-portal-hyprland hyprshot
 
