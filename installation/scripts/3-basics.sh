620d18a35ce73ea979a83f0c2e33567760b2d513
Jan Kaiser <kaiser@hey.com>
2025-07-06 23:03:34 +0200

    hypridle

--- installation/scripts/3-basics.sh
+++ installation/scripts/3-basics.sh
@@ -3,6 +3,7 @@ yay -S --noconfirm --needed \
   curl unzip inetutils openssh \
   nautilus the_silver_searcher \
   sddm qt5-graphicaleffects qt5-quickcontrols2 sddm-sugar-dark \
+  brightnessctl \
   vim tig code \
   postman-bin teams-for-linux slack-desktop bruno-bin \
   fastfetch btop htop cowsay \
