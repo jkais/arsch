b6b65b5639b082163c6aa0624c85cbf93680495c
Jan Kaiser <kaiser@hey.com>
2025-07-06 16:34:00 +0200

    little cleanup, trying to customize sddm theme

--- installation/scripts/2-identity.sh
+++ installation/scripts/2-identity.sh
@@ -2,6 +2,14 @@
 yay -S --noconfirm --needed gum
 
 # Configure identification
-echo -e "\nEnter identification for Git:"
+echo -e "\nEnter identification for Git (leave empty when already done):"
 export USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
 export USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
+
+if [[ -n "${USER_NAME//[[:space:]]/}" ]]; then
+  git config --global user.name "$USER_NAME"
+fi
+
+if [[ -n "${USER_EMAIL//[[:space:]]/}" ]]; then
+  git config --global user.email "$USER_EMAIL"
+fi
--- installation/scripts/zzz-config.sh
+++ installation/scripts/zzz-config.sh
@@ -1,11 +1,3 @@
-if [[ -n "${USER_NAME//[[:space:]]/}" ]]; then
-  git config --global user.name "$USER_NAME"
-fi
-
-if [[ -n "${USER_EMAIL//[[:space:]]/}" ]]; then
-  git config --global user.email "$USER_EMAIL"
-fi
-
 # Copy all config files to .config
 SOURCE_DIR="$HOME/.local/share/arsch/config"
 CONFIG_DIR="$HOME/.config"
