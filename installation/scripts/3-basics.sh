88f1e31ce5cce7927b83a6a19cfa1b94390c2dd3
Jan Kaiser <kaiser@hey.com>
2025-07-07 12:52:18 +0200

    jq please

--- installation/scripts/3-basics.sh
+++ installation/scripts/3-basics.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
   man less whois \
-  curl unzip inetutils openssh \
+  curl unzip inetutils openssh jq \
   nautilus the_silver_searcher \
   sddm qt5-graphicaleffects qt5-quickcontrols2 sddm-sugar-dark \
   brightnessctl \
