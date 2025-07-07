0e4411885255794580096274302f53940cbe274e
Jan Kaiser <kaiser@hey.com>
2025-07-07 19:20:48 +0200

    forgot to autoinstall alacritty and firefox

--- installation/scripts/3-basics.sh
+++ installation/scripts/3-basics.sh
@@ -1,10 +1,11 @@
 yay -S --noconfirm --needed \
-  man less whois \
+  alacritty man less whois \
   curl unzip inetutils openssh jq \
   nautilus the_silver_searcher \
   sddm qt5-graphicaleffects qt5-quickcontrols2 sddm-sugar-dark \
   brightnessctl \
-  vim tig code \
+  vim tig \
+  firefox code \
   postman-bin teams-for-linux slack-desktop bruno-bin \
   fastfetch btop htop cowsay \
   spotify \
