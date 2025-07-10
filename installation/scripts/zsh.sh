395961488f90d0a7aa83d449025d90979327cc69
Jan Kaiser <kaiser@hey.com>
2025-07-10 09:16:15 +0200

    prepare alacritty for zsh with oh-my-zsh

---
+++ installation/scripts/zsh.sh
@@ -0,0 +1,6 @@
+yay -S --noconfirm --needed blueberry > /dev/null
+
+# install oh my zsh
+export RUNZSH=no
+export CHSH=no
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
