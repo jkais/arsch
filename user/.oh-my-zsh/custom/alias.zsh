f724212605460074fbf026354689aed4c89a14cd
Jan Kaiser <kaiser@hey.com>
2025-07-11 00:01:25 +0200

    more aliases

--- user/.oh-my-zsh/custom/alias.zsh
+++ user/.oh-my-zsh/custom/alias.zsh
@@ -1,1 +1,10 @@
-alias t=tig
+alias t="tig"
+
+alias v="vim"
+
+alias gp="git pull"
+alias gu="git up"
+alias gs="git status"
+alias gd="git diff"
+
+alias update="get -qO- https://arsch.n1h0n.de | sh"
