18af87827fda85f242a45ec72151a14b4cfe38ff
Jan Kaiser <kaiser@hey.com>
2025-07-10 21:54:37 +0200

    asdf

--- installation/scripts/3-basics.sh
+++ installation/scripts/3-basics.sh
@@ -4,7 +4,7 @@ yay -S --noconfirm --needed \
   nautilus the_silver_searcher \
   sddm qt5-graphicaleffects qt5-quickcontrols2 sddm-sugar-dark \
   brightnessctl \
-  vim tig \
+  vim tig asdf-vm \
   firefox vlc ffmpeg code \
   postman-bin teams-for-linux slack-desktop bruno-bin \
   fastfetch btop htop cowsay asciiquarium \
