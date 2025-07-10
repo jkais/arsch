030fb330b0b31529fcc5eb44b6ba27177303ca96
Jan Kaiser <kaiser@hey.com>
2025-07-10 23:19:01 +0200

    test new config copy

--- installation/scripts/zzz-config.sh
+++ installation/scripts/zzz-config.sh
@@ -1,9 +1,7 @@
-# Copy all config files to .config
+# Copy all user files to /
 SOURCE_DIR=""
 CONFIG_DIR="$HOME/"
-cp -r "$HOME/.local/share/arsch/user/*" "$HOME/" > /dev/null
+cp -r "$HOME/.local/share/arsch/user/." "$HOME/" > /dev/null
 
 # Copy all root files to /
-SOURCE_DIR="$HOME/.local/share/arsch/root"
-CONFIG_DIR="/"
-sudo cp -r "$SOURCE_DIR"/* "$CONFIG_DIR" > /dev/null
+sudo cp -r "$HOME/.local/share/arsch/root/." / > /dev/null
