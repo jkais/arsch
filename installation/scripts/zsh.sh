87c80ff9d5758268bf3bf918f7ceb2f74d36c959
Jan Kaiser <kaiser@hey.com>
2025-07-10 09:19:25 +0200

    only try to install oh-my-zsh if it's not already there

--- installation/scripts/zsh.sh
+++ installation/scripts/zsh.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed zsh > /dev/null
 
 # install oh my zsh
-export RUNZSH=no
-export CHSH=no
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
