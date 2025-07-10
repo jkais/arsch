b5f0331d31b66e28f4c8ee44f57bc3559b508400
Jan Kaiser <kaiser@hey.com>
2025-07-10 23:51:37 +0200

    source zsh

--- installation/scripts/zsh.sh
+++ installation/scripts/zsh.sh
@@ -4,3 +4,5 @@ yay -S --noconfirm --needed zsh > /dev/null
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+source "$HOME/.zshrc"
