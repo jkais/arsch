45100886520ee026f7c6515a190fa7ad38d10bef
Jan Kaiser <kaiser@hey.com>
2025-07-11 23:01:49 +0200

    cleanup powerlevel10k installation

--- installation/scripts/zsh.sh
+++ installation/scripts/zsh.sh
@@ -12,4 +12,8 @@ if [ -n "$ZSH_VERSION" ]; then
 fi
 
 # install powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+POWERLEVEL10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+if [ ! -d "$POWERLEVEL10K_DIR" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL10K_DIR" > /dev/null
+fi
--- installation/scripts/zzz-config.sh
+++ installation/scripts/zzz-config.sh
@@ -3,11 +3,3 @@ cp -r "$HOME/.local/share/arsch/user/." "$HOME/" > /dev/null
 
 # Copy all root files to /
 sudo cp -r "$HOME/.local/share/arsch/root/." / > /dev/null
-
-POWERLEVEL10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-echo $POWERLEVEL10K_DIR
-
-if [ ! -d "$POWERLEVEL10K_DIR" ]; then
-  echo "YO!"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL10K_DIR" > /dev/null
-fi
