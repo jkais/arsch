382f86f0057bbc6766971b62e001095717f543d3
Jan Kaiser <kaiser@hey.com>
2025-07-11 14:41:04 +0200

    add powerlevel10k font

--- installation/scripts/fonts.sh
+++ installation/scripts/fonts.sh
@@ -2,6 +2,7 @@ yay -S --noconfirm --needed \
   ttf-dejavu ttf-liberation noto-fonts noto-fonts-emoji \
   ttf-font-awesome ttf-material-design-icons \
   noto-fonts-cjk adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts \
+  ttf-meslo-nerd-font-powerlevel10k \
   fontconfig \
   > /dev/null
 
