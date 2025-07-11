4dfca1d8ba162fd9b2c73f7e3e2be232436d92dc
Jan Kaiser <kaiser@hey.com>
2025-07-11 23:26:55 +0200

    remove some of the > /dev/null stuff

--- installation/scripts/1-yay.sh
+++ installation/scripts/1-yay.sh
@@ -1,9 +1,7 @@
-echo "Installing yay..."
-
 sudo pacman -S --needed --noconfirm base-devel &> /dev/null
 
 if ! command -v yay &>/dev/null; then
-  git clone https://aur.archlinux.org/yay-bin.git > /dev/null
+  git clone https://aur.archlinux.org/yay-bin.git
   cd yay-bin
   makepkg -si --noconfirm > /dev/null
   cd ~
--- installation/scripts/2-identity.sh
+++ installation/scripts/2-identity.sh
@@ -1,10 +1,10 @@
 # Need gum to query for input
-yay -S --noconfirm --needed gum > /dev/null
+yay -S --noconfirm --needed gum
 
 # Configure identification
 echo -e "\nEnter identification for Git (leave empty when already done):"
-export USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
-export USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
+export USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name: ")
+export USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email: ")
 
 if [[ -n "${USER_NAME//[[:space:]]/}" ]]; then
   git config --global user.name "$USER_NAME"
