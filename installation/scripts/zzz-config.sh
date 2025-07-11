e757099520834bf8258361168de5d7e1967d0d71
Jan Kaiser <kaiser@hey.com>
2025-07-11 22:51:39 +0200

    only clone powerlevel10k if it doesn't exist yet

--- installation/scripts/zzz-config.sh
+++ installation/scripts/zzz-config.sh
@@ -3,3 +3,9 @@ cp -r "$HOME/.local/share/arsch/user/." "$HOME/" > /dev/null
 
 # Copy all root files to /
 sudo cp -r "$HOME/.local/share/arsch/root/." / > /dev/null
+
+POWERLEVEL10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+if [ ! -d "$POWERLEVEL10K_DIR" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL10K_DIR" > /dev/null
+fi
