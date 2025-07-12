a0cc7c11d3d3327e08bfc2bb3d418a352b6d058e
Jan Kaiser <kaiser@hey.com>
2025-07-12 12:00:47 +0200

    add catppuccin and telescope to nvim

--- user/.config/nvim/init.lua
+++ user/.config/nvim/init.lua
@@ -7,3 +7,12 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 require("config.lazy")
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
