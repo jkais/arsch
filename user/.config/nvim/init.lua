b8f9126fb0b633102681b86d0ca1d9ce6c6c84f0
Jan Kaiser <kaiser@hey.com>
2025-07-12 15:07:43 +0200

    fixed telescope configfixed telescope config

--- user/.config/nvim/init.lua
+++ user/.config/nvim/init.lua
@@ -13,8 +13,17 @@ require("config.lazy")
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
 
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = { "^.git/" },
+  },
+}
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+
+vim.keymap.set('n', '<C-p>', function()
+  builtin.find_files({ hidden = true })
+end, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
