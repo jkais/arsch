85b6ddf85de00da72c7a0c8a751d7e2c7ea0544b
Jan Kaiser <kaiser@hey.com>
2025-07-12 15:42:40 +0200

    forgot to save asdf contents...

---
+++ installation/scripts/asdf.sh
@@ -0,0 +1,12 @@
+/usr/bin/asdf plugin add golang
+/usr/bin/asdf install golang latest
+/usr/bin/asdf set -u golang latest
+
+/usr/bin/asdf plugin add nodejs
+/usr/bin/asdf install nodejs latest
+/usr/bin/asdf set -u nodejs latest
+
+/usr/bin/asdf plugin add ruby
+/usr/bin/asdf install ruby latest
+/usr/bin/asdf set -u ruby latest
+
