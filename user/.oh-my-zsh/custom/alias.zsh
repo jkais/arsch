1b0e5ca1f3b60a3304e267d7a7f4ad6dc29cc3fd
Jan Kaiser <jan.kaiser@telefonica.com>
2025-07-23 10:44:09 +0200

    fixed vpn url

--- user/.oh-my-zsh/custom/alias.zsh
+++ user/.oh-my-zsh/custom/alias.zsh
@@ -13,4 +13,4 @@ alias gd="git diff"
 
 alias update="wget -qO- https://arsch.n1h0n.de | sh"
 
-alias vpn="sudo openconnect --background --quiet --protocol=gp go.sg.de.o2.com"
+alias vpn="sudo openconnect --background --quiet --protocol=gp go-disi.sg.de.o2.com"
