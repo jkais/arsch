30feed5ffce5ceb62f0f4be7a5efd49baeee4feb
Jan Kaiser <kaiser@hey.com>
2025-07-25 19:50:28 +0200

    neovim: Added neo tree, devicons

--- user/.config/nvim/init.lua
+++ user/.config/nvim/init.lua
@@ -19,6 +19,16 @@ require('telescope').setup {
   },
 }
 
+require('neo-tree').setup {
+  filesystem = {
+    filtered_items = {
+      visible = true, 
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    },
+  },
+}
+
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', function()
@@ -27,3 +37,4 @@ end, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<C-n>', '<Cmd>Neotree toggle<CR>')
