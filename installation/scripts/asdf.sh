6cc6e7009865d237714ba3893744f8b2834390b7
Jan Kaiser <kaiser@hey.com>
2025-07-27 01:06:45 +0200

    Revert "pin asdf node for now as it breaks" wireguard issues
    This reverts commit 49b2ef8361e28b14e5976ffa19174fdd9eef9731.

--- installation/scripts/asdf.sh
+++ installation/scripts/asdf.sh
@@ -3,8 +3,8 @@
 /usr/bin/asdf set -u golang latest
 
 /usr/bin/asdf plugin add nodejs
-/usr/bin/asdf install nodejs 24.4.0
-/usr/bin/asdf set -u nodejs 24.4.0
+/usr/bin/asdf install nodejs latest
+/usr/bin/asdf set -u nodejs latest
 
 /usr/bin/asdf plugin add ruby
 /usr/bin/asdf install ruby latest
