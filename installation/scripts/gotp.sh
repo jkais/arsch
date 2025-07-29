17e02ed9c22f46b86bbe553baca5ac0764475012
Jan Kaiser <jan.kaiser@telefonica.com>
2025-07-29 17:36:50 +0200

    fixed wget gotp

--- installation/scripts/gotp.sh
+++ installation/scripts/gotp.sh
@@ -1,3 +1,3 @@
-sudo wget -q --show-progress -O gotp https://github.com/jkais/gotp/releases/latest/download/gotp /usr/local/bin/gotp
+sudo wget -q --show-progress -O /usr/local/bin/gotp https://github.com/jkais/gotp/releases/latest/download/gotp
 sudo chmod +x /usr/local/bin/gotp
 
