6a411eacb2646b7e851552f7c80b6173f557c9f2
Jan Kaiser <kaiser@hey.com>
2025-08-01 23:22:59 +0200

    pacman easter egg

--- installation/scripts/zzz-config.sh
+++ installation/scripts/zzz-config.sh
@@ -3,3 +3,6 @@ cp -r "$HOME/.local/share/arsch/user/." "$HOME/" > /dev/null
 
 # Copy all root files to /
 sudo cp -r "$HOME/.local/share/arsch/root/." / > /dev/null
+
+# Show Pacman easter egg in pacman
+sudo grep -q '^ILoveCandy$' /etc/pacman.conf || sudo sed -i '/^\[options\]/a ILoveCandy' /etc/pacman.conf
