570937de3d835597571f5cc9457bd0be2b3732cd
Jan Kaiser <kaiser@hey.com>
2025-08-02 13:09:51 +0200

    switch from mako to swaync

--- installation/scripts/hyprland.sh
+++ installation/scripts/hyprland.sh
@@ -2,7 +2,7 @@ yay -S --noconfirm --needed \
   hyprland \
   hyprlock hyprpaper hyprshot hypridle \
   hyprpicker hyprsysteminfo \
-  mako loupe \
+  swaync loupe \
   waybar wayland-protocols wl-clipboard \
   wlroots wofi xdg-desktop-portal-hyprland \
   > /dev/null
