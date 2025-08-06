f05a198ae86e79cfa7b1b72112d24251093d2372
Jan Kaiser <kaiser@hey.com>
2025-08-06 21:28:11 +0200

    added deno, because peek.vim wants to have it...

--- installation/scripts/zzzz-asdf.sh
+++ installation/scripts/zzzz-asdf.sh
@@ -10,6 +10,14 @@
 /usr/bin/asdf install ruby latest
 /usr/bin/asdf set -u ruby latest
 
+/usr/bin/asdf plugin add python
+/usr/bin/asdf install python latest
+/usr/bin/asdf set -u python latest
+
+/usr/bin/asdf plugin add deno
+/usr/bin/asdf install deno latest
+/usr/bin/asdf set -u deno latest
+
 # Install LSPs for Neovim
 $HOME/.asdf/shims/npm install -g typescript typescript-language-server
 $HOME/.asdf/shims/gem install solargraph
