bdc2229ed2b54df655d8ff2c7fb79fa09efd7c7a
Jan Kaiser <kaiser@hey.com>
2025-08-09 10:41:41 +0200

    added network manager, did some resorting

--- installation/scripts/2-basics.sh
+++ installation/scripts/2-basics.sh
@@ -1,25 +1,30 @@
 yay -S --noconfirm --needed \
-  alacritty man less whois \
-  wl-clipboard cliphist \
-  okular evince \
+	man less whois \
+	networkmanager iw bind \
+  openconnect wireguard-tools systemd-resolvconf \
   plymouth \
-  curl unzip inetutils openssh jq bind \
-  zoxide bat fzf the_silver_searcher gum \
   sddm qt5-graphicaleffects qt5-quickcontrols2 sddm-sugar-dark \
+  curl unzip inetutils openssh jq \
+  alacritty \
+  wl-clipboard cliphist \
+  zoxide bat fzf the_silver_searcher gum \
+  vim neovim luarocks \
+	tig asdf-vm \
+  btop htop \
   playerctl pamixer pavucontrol \
   brightnessctl power-profiles-daemon \
-  vim neovim luarocks tig asdf-vm \
   nautilus firefox pinta \
   chromium libreoffice-fresh \
   vlc ffmpeg ffmpegthumbnailer gnome-video-thumbnailer \
   code \
   git-crypt \
   postman-bin teams-for-linux slack-desktop bruno-bin \
-  btop htop \
   fastfetch cowsay asciiquarium sl cbonsai \
-  iw \
-  openconnect wireguard-tools systemd-resolvconf \
   spotify \
+  okular evince \
 
 sudo systemctl enable sddm
+sudo systemctl start sddm
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
 xdg-settings set default-web-browser firefox.desktop
