a295ca13a1ee2b31e49d287fac9d85207126ca75
Jan Kaiser <jan.kaiser@telefonica.com>
2025-09-10 17:31:34 +0200

    removed all the webapps that I do not use regularly

--- installation/scripts/webapps.sh
+++ installation/scripts/webapps.sh
@@ -35,9 +35,6 @@ EOF
 }
 
 web2app "Discord" https://discord.com/channels/@me https://w7.pngwing.com/pngs/1023/637/png-transparent-discord-hd-logo-thumbnail.png
-web2app "HEY" https://app.hey.com https://www.hey.com/assets/images/general/hey.png
-web2app "WhatsApp" https://web.whatsapp.com/ https://medi-suedwest.de/wp-content/uploads/2018/10/whatsapp-official-logo-png-download.png
 web2app "ChatGPT" https://chatgpt.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
-web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 web2app "Perplexity" https://www.perplexity.ai/ https://aws.amazon.com/startups/upload/e4d8d468-90d1-704f-a34e-7e195ce4025a/ceac2e85-dca6-4da4-bbf7-359df7db739d.png
 web2app "Claude" https://claude.ai/new https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/claude-ai-icon.png
